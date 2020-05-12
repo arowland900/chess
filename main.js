a4a1e329292bd3b4a0ea13bf4ace180a55694588
Alexander Rowland <arowland900@gmail.com>
2020-05-12 11:23:53 -0700

    add piece move function (handleClick)

--- main.js
+++ main.js
@@ -24,15 +24,15 @@ let movements = {
 
 /*----- app's state (variables) -----*/
 let board = [
-    ['wR','wKn','wB','wK','wQ','wB','wKn','wR',],
-    ['wP','wP','wP','wP','wP','wP','wP','wP',],
-    [null,null,null,null,null,null,null,null,],
-    [null,null,null,null,null,null,null,null,],
-    [null,null,null,null,null,null,null,null,],
-    [null,null,null,null,null,null,null,null,],
-    ['bP','bP','bP','bP','bP','bP','bP','bP',],
-    ['bR','bKn','bB','bQ','bK','bB','bKn','bR',],
-  ]
+    ['wR', 'wKn', 'wB', 'wK', 'wQ', 'wB', 'wKn', 'wR',],
+    ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP',],
+    [null, null, null, null, null, null, null, null,],
+    [null, null, null, null, null, null, null, null,],
+    [null, null, null, null, null, null, null, null,],
+    [null, null, null, null, null, null, null, null,],
+    ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP',],
+    ['bR', 'bKn', 'bB', 'bQ', 'bK', 'bB', 'bKn', 'bR',],
+]
 let turn = 1
 let movingPiece = null
 /*----- cached element references -----*/
@@ -40,55 +40,55 @@ let gameBoard = document.getElementById('board')
 
 /*----- event listeners -----*/
 document.getElementById('board')
-  .addEventListener('click', handleClick);
+    .addEventListener('click', handleClick);
 /*----- functions -----*/
 
 
 
 init()
 
-function init(){
-    
+function init() {
+
     appendBoard()
-    render()
+    // render()
 }
 
-function handleClick(e){
+function handleClick(e) {
     let i = e.target.id.split('')[1]
     let j = e.target.id.split('')[3]
     let piece = board[i][j]
-    
-    if(piece){
-        movingPiece = piece
-        console.log('movingPiece: ', movingPiece)
+    if (piece) {
+        movingPiece = [piece, i, j]
+    } else if (movingPiece) {
+        checkValidMove(movingPiece, piece)
+        board[i][j] = movingPiece[0]
+        board[movingPiece[1]][movingPiece[2]] = null
+        movingPiece = null
     } else {
-        if(movingPiece){
-            console.log('place to move: ', i,j)
-            // checkValidity(movingPiece, i, j)
-            movingPiece = null
-        } else {
-            console.log('no piece to move')
-            return
-        }
+        console.log('no piece to move')
+        return
     }
+    appendBoard()
+}
+
+function checkValidMove(piece, desiredSquare){
+    
 }
 
 
-function appendBoard(){
-    board.forEach((e,i) => {
+function appendBoard() {
+    gameBoard.textContent = ''
+    board.forEach((e, i) => {
         e.forEach((f, j) => {
             let square = document.createElement('div')
             square.setAttribute('id', `c${i}r${j}`)
             square.setAttribute('class', 'square')
-            let x = (j + i) %2
-            x ? square.style.backgroundColor = 'brown' 
-            : square.style.backgroundColor = 'beige'
+            let x = (j + i) % 2
+            x ? square.style.backgroundColor = 'brown'
+                : square.style.backgroundColor = 'beige'
             square.textContent = pieceIcons[f]
             gameBoard.appendChild(square)
         })
     })
 }
 
-function render(){
-
-}
