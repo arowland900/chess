64a25c997df299a8230a315748c0d74addaa9854
Alexander Rowland <arowland900@gmail.com>
2020-05-12 17:17:48 -0700

    add turn & moving & capture logic for pawns

--- main.js
+++ main.js
@@ -37,8 +37,6 @@ document.getElementById('board')
 reset.addEventListener('click', init);
 /*----- functions -----*/
 
-
-
 init()
 
 function init() {
@@ -57,13 +55,11 @@ function init() {
 }
 
 function handleClick(e) {
-    let i = e.target.id.split('')[1]
-    let j = e.target.id.split('')[3]
+    let i = Number(e.target.id.split('')[1])
+    let j = Number(e.target.id.split('')[3])
     piece = board[i][j]
     let desired = [i, j]
 
-
-    console.log("PIECE: ", piece)
     if (piece && movingPiece == null) {
         movingPiece = [piece, i, j]
     } else if (movingPiece) {
@@ -72,6 +68,8 @@ function handleClick(e) {
             board[i][j] = movingPiece[0]
             board[movingPiece[1]][movingPiece[2]] = null
             movingPiece = null
+            piece = null
+            turn *= -1
         } else {
             movingPiece = [piece, i, j]
             piece = null
@@ -86,50 +84,55 @@ function handleClick(e) {
 
 function checkValidMove(moving, desired) {
     // black pawn logic
-    if (moving[0] === 'bP') {
-        if (board[desired[0]][desired[1]] === null) {
-            if (moving[1] - 1 == desired[0] && moving[2] == desired[1]) {
-                // console.log("WE'VE GOT A VALID MOVE!")
-                return true
-            } else if (moving[1] == 6 && desired[0] == 4 && moving[2] == desired[1]) {
-                return true
-            }
-
-        } else {
-            if (board[desired[0]][desired[1]].split('')[0] == 'b') {
-                // console.log("same team, invalid move!")
-                return false
-            }
-            if (board[desired[0]][desired[1]].split('')[0] == 'w') {
-                // console.log("Opponent")
-                if (moving[1] - 1 == desired[0] && (moving[2] - 1 == desired[1] || moving[2] + 1 == desired[1])) {
-                    // console.log("CAPTURED!")
+    if (turn === -1) {
+        if (moving[0] === 'bP') {
+            if (board[desired[0]][desired[1]] === null) {
+                if (moving[1] - 1 == desired[0] && moving[2] == desired[1]) {
+                    console.log("WE'VE GOT A VALID MOVE!")
                     return true
+                } else if (moving[1] == 6 && desired[0] == 4 && moving[2] == desired[1]) {
+                    return true
+                }
+
+            } else {
+                if (board[desired[0]][desired[1]].split('')[0] == 'b') {
+                    console.log("same team, invalid move!")
+
+                    return false
+                }
+                if (board[desired[0]][desired[1]].split('')[0] == 'w') {
+                    console.log("Opponent")
+                    if (Number(moving[1]) - 1 == desired[0] && (moving[2] - 1 == desired[1] || moving[2] + 1 == desired[1])) {
+                        console.log("CAPTURED!")
+                        return true
+                    }
                 }
             }
         }
     }
     // white pawn logic
-    if (moving[0] === 'wP') {
-        if (board[desired[0]][desired[1]] === null) {
-            if (Number(moving[1]) + 1 == desired[0] && moving[2] == desired[1]) {
-                // console.log("WE'VE GOT A VALID MOVE!")
-                return true
-            } else if (moving[1] == 1 && desired[0] == 3 && moving[2] == desired[1]) {
-                return true
-            }
-
-        } else {
-            if (board[desired[0]][desired[1]].split('')[0] == 'w') {
-                // console.log("same team, invalid move!")
-                return false
-            }
-            if (board[desired[0]][desired[1]].split('')[0] == 'b') {
-
-                // console.log("Opponent")
-                if (Number(moving[1]) + 1 == desired[0] && (moving[2] - 1 == desired[1] || Number(moving[2]) + 1 == desired[1])) {
-                    // console.log("CAPTURED!")
+    if (turn === 1) {
+        if (moving[0] === 'wP') {
+            if (board[desired[0]][desired[1]] === null) {
+                if (Number(moving[1]) + 1 == desired[0] && moving[2] == desired[1]) {
+                    // console.log("WE'VE GOT A VALID MOVE!")
                     return true
+                } else if (moving[1] == 1 && desired[0] == 3 && moving[2] == desired[1]) {
+                    return true
+                }
+
+            } else {
+                if (board[desired[0]][desired[1]].split('')[0] == 'w') {
+                    // console.log("same team, invalid move!")
+                    return false
+                }
+                if (board[desired[0]][desired[1]].split('')[0] == 'b') {
+
+                    // console.log("Opponent")
+                    if (Number(moving[1]) + 1 == desired[0] && (moving[2] - 1 == desired[1] || Number(moving[2]) + 1 == desired[1])) {
+                        // console.log("CAPTURED!")
+                        return true
+                    }
                 }
             }
         }
